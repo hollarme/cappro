1464569f7a7aa1f6da67cd589c428e18adeeb550
EniolaAkin <eniola.akinboboye@gmial.com>
2024-03-22 02:27:46 +0100

    first commit

---
+++ .ipynb_checkpoints/form-checkpoint.py
@@ -0,0 +1,65 @@
+import streamlit as st
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+from utils import get_data, get_all_data, put_data, init_connection
+
+st.set_page_config(page_title="Capstone Project Tool", layout="wide") 
+
+if 'client' not in st.session_state:
+    st.session_state['client'] = init_connection()
+    
+if 'db' not in st.session_state:
+    st.session_state['db'] = st.session_state['client'].capstoneDB
+
+# if 'collection' not in st.session_state:
+#     st.session_state['collection'] = 'title_information_form'
+    
+if 'collection' not in st.session_state:
+    st.session_state['collection'] = 'student_staff_map'
+
+
+col1, col2 = st.columns([0.7,0.3])
+
+with col1:
+    st.title("Capstone Project Information Form")
+
+    # with st.form('info_form'):
+    with st.container(border=True):
+        matric_number = st.text_input('Enter your matric number', placeholder='XXX/YYYY/ZZZ', max_chars=12, key='matric')
+
+        full_name = st.text_input('Enter your full name (surname first)', value="" if not matric_number else get_data(matric_number).get('Names', ""), key='full')
+
+        title = st.text_input('Enter the title of your project work', value="" if not matric_number else get_data(matric_number).get('Title', ""), key='title')
+
+        opts = ["",'Mr. Olorunniwo', 'Dr. Aransiola', 'Dr. Obayiuwana', 'Dr. Yesufu', 'Dr. Ariyo', 'Dr. Ogunseye', 'Mr. Olayiwola', 'Dr. Ayodele', 'Dr. Ilori', 'Mr. Akinboboye', 'Dr. Olawole', 'Dr. Babalola', 'Dr. Ogunba', 'Dr. Fisusi', 'Dr. Jubril']
+
+        supervisor = st.selectbox('Who is your supervisor?', options=opts, index=0 if not matric_number else opts.index(get_data(matric_number).get('Supervisor', "").title()), key='super')
+        
+        option = get_data(matric_number).get('Option', "")
+        group = get_data(matric_number).get('Group', "")
+        assessors = get_data(matric_number).get('Assessors', "")
+
+        readiness = st.slider('How ready are you for a presentation?', min_value=0, max_value=10, value=0 if not matric_number else get_data(matric_number).get('Readiness', 0), key='read')
+
+        # submitted = st.form_submit_button("Submit")
+        submitted = st.button("Submit")
+
+        if submitted:
+            if not all([matric_number=="", full_name=="", title=="", supervisor==""]):
+                put_data(matric_number, {"_id": matric_number, "Title": title, "Names": full_name, "Supervisor": supervisor, "Option": option, "Group": group, "Assessors": assessors, "Readiness": readiness})
+            else:
+                st.warning('Make sure all the fields are properly filled', icon="⚠️")
+
+            
+with col2:  
+    st.title("Readiness at a glance!")
+    arr = []
+    
+    for x in get_all_data():
+        arr.append(x.get('Readiness', 0))
+        
+    fig, ax = plt.subplots()
+    ax.hist(arr, bins=11, align='mid')
+
+    st.pyplot(fig)
---
+++ utils.py
@@ -0,0 +1,43 @@
+import streamlit as st
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+# (kibtool) imachine@imachine:~$ mongosh "mongodb+srv://cluster0.lhxokzx.mongodb.net/" --apiVersion 1 --username bovenssolutions
+# st.write(st.secrets["mongo"]["uri"])
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    # Create a new client and connect to the server
+    return MongoClient(str(st.secrets["mongo"]["uri"]), server_api=ServerApi('1'))
+    # return pymongo.MongoClient(**st.secrets["mongo"], authSource="admin")
+
+
+# Pull data from the collection.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+# @st.cache_data(ttl=600)
+def get_data(uid):
+    db = st.session_state['db']
+    collection = st.session_state['collection']
+    item = db[collection].find_one({'_id':uid})
+    return item if item else {}
+
+def get_all_data():
+    db = st.session_state['db']
+    collection = st.session_state['collection']
+    
+    items = db[collection].find({},{'_id':0})
+    items = list(items)  # make hashable for st.cache_data
+    return items if items else {}
+
+
+# Pull data from the collection.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+# @st.cache_data(ttl=600)
+def put_data(uid, data):
+    db = st.session_state['db']
+    collection = st.session_state['collection']
+    db[collection].replace_one({'_id':uid},data,True)
+
