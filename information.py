c2cb3a236f2d5ed0b8db6fea131f2c934f6c2543
EniolaAkin <eniola.akinboboye@gmial.com>
2024-04-09 10:04:20 +0100

    added information

--- information.py
+++ information.py
@@ -21,13 +21,25 @@ st.header("Information Board")
 with st.container(border=True):
 
     st.markdown("""
+    ## General Information:
+    
     - Please be aware that :red[EEE501 defense] for the 2022/2023 academic session will take place on :red[Tuesday $16^{th}$ and Wednesday $17^{th}$] of the month of April 2024
     - It shall take the form of a poster presentation (see the poster template below) where the assessors and guests will have the chance to interact with the presenters
-    - This is the link to the poster template (look for the poster named :red[Kensington.pptx]): https://www.posterpresentations.com/free-poster-templates.html
     - You will also need to write a concise report (3 to 5 pages) on your work (see the report link below)
+    - Both the report and the poster are to be submitted with file upload menu, on or before :red[midnight Thursday $11^{th}$ of April 2024].
+    - Each poster presentation transaction between an assessor and the student should not take more than 10 mins: 5 minutes for the presentation and 5 for questions and answers
+    - You are required to print the poster on an A1 paper
+    
+    ## Material Links:
+    
+    - This is the link to the poster template (look for the poster named: :red[Kensington.pptx]): https://www.posterpresentations.com/free-poster-templates.html
     - This is the link to the report template: https://drive.google.com/file/d/1yLS-4gloZyNICn6eAJjDnM-AdGSjKSes/view?usp=sharing
     - You might need to compile the latex output with TexMaker (https://www.xm1math.net/texmaker/) or any other tool you are comfortable with
-    - Both the report and the poster are to be submitted with file upload menu, on or before :red[midnight Thursday $11^{th}$ of April 2024].
+
+    ## Poster Presentation How-to: 
+    
+    - https://www.youtube.com/watch?v=vMSaFUrk-FA
+    - https://www.youtube.com/watch?v=r0ezNEDWAiE
     """)
 
    
